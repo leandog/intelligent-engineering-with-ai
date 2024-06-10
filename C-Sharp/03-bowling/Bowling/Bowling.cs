bfb3aa27d56746cded9efb65fbbab0a26b1dcead
DEV3L <jus.beall@gmail.com>
2024-06-10 01:10:37 -0400

    Add solution for Bowling Kata

--- C-Sharp/03-bowling/Bowling/Bowling.cs
+++ C-Sharp/03-bowling/Bowling/Bowling.cs
@@ -1,4 +1,129 @@
 ﻿namespace Bowling
 {
-    public class Bowling { }
+    /// <summary>
+    /// Represents a bowling game and provides functionality to calculate the score.
+    /// </summary>
+    public class BowlingGame
+    {
+        private const int StrikeScore = 10;
+        private const int SpareScore = 10;
+        private const int MissScore = 0;
+        private const int MaxFrames = 10;
+        private const int FrameRolls = 2;
+
+        private static bool IsStrike(string frame) => frame == "X";
+
+        private static bool IsSpare(string frame) => frame.Length == FrameRolls && frame[1] == '/';
+
+        /// <summary>
+        /// Calculates the total score for a given sequence of rolls.
+        /// </summary>
+        /// <param name="rolls">The sequence of rolls in a single string.</param>
+        /// <returns>The total score for the game.</returns>
+        /// <exception cref="ArgumentException">Thrown when an invalid roll character is encountered.</exception>
+        public int GetScore(string rolls)
+        {
+            int score = 0;
+            var frames = rolls.Split(' ');
+
+            for (
+                int frameIndex = 0;
+                frameIndex < frames.Length && frameIndex < MaxFrames;
+                frameIndex++
+            )
+            {
+                var fullFrame = frames[frameIndex];
+                var frame = fullFrame[..Math.Min(FrameRolls, fullFrame.Length)];
+
+                score += CalculateFrameScore(frames, frameIndex, frame);
+
+                // Handle bonus rolls in the 10th frame
+                if (frameIndex == MaxFrames - 1 && fullFrame.Length > FrameRolls)
+                {
+                    score += CalculateBonusRolls(fullFrame);
+                }
+            }
+
+            return score;
+        }
+
+        private int CalculateFrameScore(string[] frames, int frameIndex, string frame)
+        {
+            if (IsStrike(frame))
+            {
+                return CalculateStrikeScore(frames, frameIndex);
+            }
+
+            if (IsSpare(frame))
+            {
+                return CalculateSpareScore(frames, frameIndex);
+            }
+
+            return CalculateNormalFrameScore(frame);
+        }
+
+        private int CalculateStrikeScore(string[] frames, int frameIndex)
+        {
+            if (frameIndex + 1 >= frames.Length)
+            {
+                return StrikeScore;
+            }
+
+            int score = StrikeScore;
+
+            var nextFrame = frames[frameIndex + 1];
+            score += GetRollValue(nextFrame[0]);
+
+            if (nextFrame.Length > 1 && nextFrame[1] == '/')
+            {
+                score += SpareScore - GetRollValue(nextFrame[0]);
+            }
+            else if (nextFrame.Length > 1)
+            {
+                score += GetRollValue(nextFrame[1]);
+            }
+            else if (frameIndex + 2 < frames.Length)
+            {
+                var frameAfterNext = frames[frameIndex + 2];
+                score += GetRollValue(frameAfterNext[0]);
+            }
+
+            return score;
+        }
+
+        private int CalculateSpareScore(string[] frames, int frameIndex)
+        {
+            if (frameIndex + 1 >= frames.Length)
+            {
+                return SpareScore;
+            }
+
+            int score = SpareScore;
+            var nextFrame = frames[frameIndex + 1];
+            score += GetRollValue(nextFrame[0]);
+            return score;
+        }
+
+        private int CalculateNormalFrameScore(string frame)
+        {
+            return frame.Sum(GetRollValue);
+        }
+
+        private int CalculateBonusRolls(string fullFrame)
+        {
+            return fullFrame.Skip(FrameRolls).Select(GetRollValue).Sum();
+        }
+
+        private int GetRollValue(char roll)
+        {
+            return roll switch
+            {
+                'X' => StrikeScore,
+                '/' => SpareScore,
+                '-' => MissScore,
+                _ when char.IsDigit(roll) => int.Parse(roll.ToString()),
+                _ => throw new ArgumentException($"Invalid roll character: {roll}")
+            };
+        }
+    }
 }
--- C-Sharp/03-bowling/BowlingTests/BowlingTests.cs
+++ C-Sharp/03-bowling/BowlingTests/BowlingTests.cs
@@ -1,88 +1,104 @@
-/*
-# Three Laws of Test Driven Development
-- You are not allowed to write any production code unless it is to make a failing unit test pass.
-- You are not allowed to write any more of a unit test than is sufficient to fail; and compilation failures are failures.
-- You are not allowed to write any more production code than is sufficient to pass the one failing unit test.
-*/
-
-/*
-Fake it till you make it
-*/
-
-/*
-# ZOMBIES
-- Z: Zero - What happens if there are zero of a thing?
-- O: One - What happens when there is exactly one of a thing?
-- M: Many - What happens when there are more than one thing?
-- B: Boundaries - Are there special boundary values for the thing?
-- I: Interfaces - Are there common interfaces with expectations?
-- E: Exceptions/Errors - What cases cause errors? Exactly what should they do?
-*/
-
-/*
-S - Single Responsibility Principle
-O - Open / Closed => Open for extension, closed for modification
-L - Liskov Substitution => shape.area :: rectangle.area : square.area : circle.area
-I - Interface Segregation
-D - Dependency Inversion
-*/
-
-/*
-D - Don't
-R - Repeat
-Y - Yourself
-*/
-
-/*
-Y - You
-A - Ain't
-G - Gonna
-N - Need
-I - It
-*/
-
-/*
-PROBLEM REQUIREMENTS:
-Create a program, which, given a valid sequence of rolls for one line of American Ten-Pin Bowling,
-produces the total score for the game. Here are some things that the program will not do:
-
-- We will not check for valid rolls.
-- We will not check for correct number of rolls and frames.
-- We will not provide scores for intermediate frames.
-
-Depending on the application, this might or might not be a valid way to define a complete story,
-but we do it here for purposes of keeping the kata light. I think you’ll see that improvements like
-those above would go in readily if they were needed for real.
-
-We can briefly summarize the scoring for this form of bowling:
-
-- Each game, or “line” of bowling, includes ten turns, or “frames” for the bowler.
-- In each frame, the bowler gets up to two tries to knock down all the pins.
-- If in two tries, he fails to knock them all down, his score for that frame is the total number
-    of pins knocked down in his two tries.
-- If in two tries he knocks them all down, this is called a “spare” and his score for the frame
-    is ten plus the number of pins knocked down on his next throw (in his next turn).
-- If on his first try in the frame he knocks down all the pins, this is called a “strike”. His
-    turn is over, and his score for the frame is ten plus the simple total of the pins knocked down
-    in his next two rolls.
-- If he gets a spare or strike in the last (tenth) frame, the bowler gets to throw one or two more
-    bonus balls, respectively. These bonus throws are taken as part of the same turn. If the bonus throws
-    knock down all the pins, the process does not repeat: the bonus throws are only used to calculate the
-    score of the final frame.
-- The game score is the total of all frame scores.
-*/
-
+using System;
 using Xunit;
 
 namespace Bowling
 {
-    public class BowlingTests
+    public class BowlingGameTests
     {
         [Fact]
-        public void TestBowling()
+        public void GutterGameScoresZero()
         {
-            // var Bowling = new Bowling();
-            Assert.True(false, "Turn the checked value to true to pass this test");
+            var game = new BowlingGame();
+            var score = game.GetScore("-- -- -- -- -- -- -- -- -- --");
+            Assert.Equal(0, score);
+        }
+
+        [Fact]
+        public void AllNinesScoresNinety()
+        {
+            var game = new BowlingGame();
+            var score = game.GetScore("9- 9- 9- 9- 9- 9- 9- 9- 9- 9-");
+            Assert.Equal(90, score);
+        }
+
+        [Fact]
+        public void AllOnesScoresTwenty()
+        {
+            var game = new BowlingGame();
+            var score = game.GetScore("11 11 11 11 11 11 11 11 11 11");
+            Assert.Equal(20, score);
+        }
+
+        [Fact]
+        public void SingleSpareFollowedByThreeScoresSixteen()
+        {
+            var game = new BowlingGame();
+            int score = game.GetScore("5/ 3- -- -- -- -- -- -- -- --");
+            Assert.Equal(16, score);
+        }
+
+        [Fact]
+        public void SingleStrikeFollowedByThreeAndFourScoresTwentyFour()
+        {
+            var game = new BowlingGame();
+            int score = game.GetScore("X 34 -- -- -- -- -- -- -- --");
+            Assert.Equal(24, score);
+        }
+
+        [Fact]
+        public void ThreeStrikesInARowScoresSixty()
+        {
+            var game = new BowlingGame();
+            int score = game.GetScore("X X X -- -- -- -- -- -- --");
+            Assert.Equal(60, score);
+        }
+
+        [Fact]
+        public void StrikeSpareStrikeThreeAndMissScoresSixtyThree()
+        {
+            var game = new BowlingGame();
+            int score = game.GetScore("X 5/ X 3- -- -- -- -- -- --");
+            Assert.Equal(56, score);
+        }
+
+        [Fact]
+        public void TwoStrikesSpareFiveThreeScoresEightyOne()
+        {
+            var game = new BowlingGame();
+            int score = game.GetScore("X X 5/ 53 -- -- -- -- -- --");
+
+            Assert.Equal(68, score);
+        }
+
+        [Fact]
+        public void AllSparesWithZeroScoresOneFortyFiz()
+        {
+            var game = new BowlingGame();
+            int score = game.GetScore("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/0");
+            Assert.Equal(145, score);
+        }
+
+        [Fact]
+        public void AllSparesWithFiveScoresOneHundredFifty()
+        {
+            var game = new BowlingGame();
+            int score = game.GetScore("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5");
+            Assert.Equal(150, score);
+        }
+
+        [Fact]
+        public void PerfectGameScoresThreeHundred()
+        {
+            var game = new BowlingGame();
+            int score = game.GetScore("X X X X X X X X X X X X");
+            Assert.Equal(300, score);
+        }
+
+        [Fact]
+        public void InvalidRollCharacterThrowsException()
+        {
+            var game = new BowlingGame();
+            Assert.Throws<ArgumentException>(() => game.GetScore("X X X X X X X X X X X Y"));
         }
     }
 }
